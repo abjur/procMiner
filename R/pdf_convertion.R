e71f838745f82914d9807bcc9bef1331544e9fb1
azeloc <ezeloc2@gmail.com>
2016-03-17 23:37:54 -0400

    mudei tudo e agora tá bom

--- R/pdf_convertion.R
+++ R/pdf_convertion.R
@@ -1,3 +1,5 @@
+
+#' @export
 formatos <- function(orgao = '8', tr = '26'){
   paste0(
     #formato CNJ - 2008
@@ -9,7 +11,8 @@ formatos <- function(orgao = '8', tr = '26'){
   )
 }
 
-pdf2text <- function(a, keep_file = F, new_file = "repo.txt", raw = F, first_pg = NA, last_pg = NA){
+#' @export
+pdf2text <- function(a, first_pg = NA, last_pg = NA, raw = F, keep_file = F, new_file = "repo.txt"){
   bash = ifelse(raw,'pdftotext -raw','pdftotext')
   bash = ifelse(is.na(first_pg),bash,paste(bash,'-f',first_pg))
   bash = ifelse(is.na(last_pg),bash,paste(bash,'-l',last_pg))
@@ -17,44 +20,63 @@ pdf2text <- function(a, keep_file = F, new_file = "repo.txt", raw = F, first_pg 
   
   if(!file.exists(a)){return('')}
   
-  if(file.size(a) > 5000){
+  if(file.size(a) > 5000 & stringi::stri_detect(a,fixed = '.pdf')){
     system(bash)
-    text = readr::read_file(new_file)
+    texto = readr::read_file(new_file)
     ifelse(keep_file,NA,file.remove(new_file))
   } else {
-    text = ''
+    texto = ''
   }
-  return(text)
+  return(texto)
 }
 
-encontra_processos <- function(text, orgao = '8', tr = '26'){
-  text %>%
-    stringi::stri_replace_all(regex = '[\n]', replacement = '')%>%
-    stringi::stri_replace_all(regex = '\f[0-9].*de2016|[\f\r]', replacement = '') %>%
+
+encontra_processos <- function(texto, orgao = '8', tr = '26', tj = 'TJSP'){
+  texto %>%
+    tira_header_pagina(tj) %>% 
     stringi::stri_extract_all(regex = formatos(orgao,tr)) %>%
     dplyr::first() %>%
     dplyr::data_frame() %>%
-    setNames('n_processo')
+    setNames('n_processo') %>% 
+    dplyr::distinct(n_processo)
 }
 
-filtra_anos <- function(processos, anos = seq(2009,2015)){
-  processos %>%
-    plyr::adply(1,substr, start = 12, stop = 15) %>%
-    dplyr::filter(V1 %in% anos) %>%
-    dplyr::select(n_processo)
+tira_header_pagina <- function(texto, tj = 'TJSP'){
+  texto %<>% 
+    stringi::stri_replace_all(regex = '\n', replacement = '')
+  if(tj == 'TJSC'){
+    texto %<>% 
+      stringi::stri_replace_all(regex = '\f[0-9].*?de2016|[\f\r]', replacement = '')
+  } else if (tj %in% c('TJSP','TJAL')){
+    texto %<>% 
+      stringi::stri_replace_all(regex = '[\f ]', replacement = '') %>% 
+      stringi::stri_replace_all(regex = 'PublicaçãoOficialdoTribunal.*?Edição[0-9]{3,6}', replacement = '')
+  } 
+  return(texto)
 }
 
-extrai_processos_arq <- function(a,orgao = '8', tr = '26'){
-  print(a)
-  a %>%
-    pdf2text %>%
-    encontra_processos(orgao, tr)
+extrai_processos_arq <- function(a,orgao = '8', tr = '26', r = F, tj= 'TJSP', save_file = T){
+  d = a %>%
+    pdf2text(raw = r) %>%
+    encontra_processos(orgao, tr, tj)
+  
+  if(save_file){saveRDS(d,file = stringi::stri_replace(a, fixed = '.pdf', replacement = '.rds'))}
+  
+  return(d)
 }
 
-extrai_processos <- function(lista_arquivos, orgao = '8', tr = '26'){
+extrai_processos <- function(lista_arquivos, orgao = '8', tr = '26', raw = F, tj = 'TJSP'){
   dplyr::data_frame(arq = lista_arquivos) %>%
     dplyr::group_by(arq) %>%
-    dplyr::do(extrai_processos_arq(.$arq, orgao,tr)) %>%
+    dplyr::do(extrai_processos_arq(.$arq, orgao,tr,raw)) %>%
     dplyr::ungroup() %>%
     dplyr::distinct(n_processo)
+}
+
+tipo_n_procesos <- function(d){
+  d %>% 
+    dplyr::select(n_processo) %>% 
+    dplyr::first() %>% 
+    stringi::stri_length() %>% 
+    table()
 }
---
+++ data-raw/tests.R
@@ -0,0 +1,16 @@
+a1 <- 'data-raw/dje_pdf/tjsc_dje_2016-03-15/tjsc_dje_1_2016-03-15.pdf'
+a2 <- 'data-raw/dje_pdf/tjsp_dje_2016-03-16/tjsp_dje_11_2016-03-16.pdf'
+a3 <- 'data-raw/dje_pdf/tjal_dje_2016-03-17/tjal_dje_3_2016-03-17.pdf'
+
+a2 %>% pdf2text(1,1,F,T) %>% tira_header_pagina('TJSP') %>% encontra_processos()
+
+header_alagoas = 'deAlagoas -LeiFederalnº11.419/06,art.4ºDisponibilização:quinta-feira,17demarçode2016DiárioOficialPoderJudiciário-CadernoJurisdicional-PrimeiroGrauMaceió,AnoVII-Edição'
+header_sp =      'deSãoPaulo-LeiFederalnº11.419/06,art.4ºDisponibilização:quarta-feira,16demarçode2016DiáriodaJustiçaEletrônico-CadernoJudicial-2ªInstânciaSãoPaulo,AnoIX-Edição20772'
+
+lista_sp = list.files('data-raw/dje_pdf/tjsp_dje_2016-03-16/', full.names = T)
+lista_sc = list.files('data-raw/dje_pdf/tjsc_dje_2016-03-15/', full.names = T)
+lista_al = list.files('data-raw/dje_pdf/tjal_dje_2016-03-17/', full.names = T)
+
+sp = lista_sp %>% extrai_processos(raw = T)
+al = lista_al %>% extrai_processos(tr = '02', raw = T, tj = 'TJAL')
+sc = lista_sc %>% extrai_processos(tr = '24', raw = T, tj = 'TJSC')
